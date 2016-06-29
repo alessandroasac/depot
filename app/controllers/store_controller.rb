478ca49563b61729bbf70b87d28068198030fbe0
Alessandro Costa <alessandro.asac@gmail.com>
2016-06-28 22:46:37 -0300

    Chapter 9 playtime

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
   def index
+    session[:counter] = 0 if session[:counter].nil?
+    session[:counter] += 1
     @products = Product.order(:title)
   end
 end
