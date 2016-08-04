08068819040cc1aa133ff21c064a0a8450601dd8
Alessandro Costa <alessandro.asac@gmail.com>
2016-08-04 14:33:46 -0300

    Finished item 12.2

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   resources :carts
   get 'store/index'
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   root 'store#index', as: 'store'
 
