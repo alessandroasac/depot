7987de5e5a52fd5e1b4f495131f53fb358816aea
Alessandro Costa <alessandro.asac@gmail.com>
2016-08-06 22:23:36 -0300

    Finished item 13.1

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -38,6 +38,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
+        OrderNotifier.received(@order).deliver_now
 
         format.html { redirect_to store_url, notice:
           'Thank you for your order.' }
