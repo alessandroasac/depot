fd0acc5daffbedcc1f3f92e5083d7a866d1b603a
Alessandro Costa <alessandro.asac@gmail.com>
2016-08-11 21:04:00 -0300

    Removed pay_type from orders. The substitute column is payment_type_id that references PaymentType

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804192533) do
+ActiveRecord::Schema.define(version: 20160812000135) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 20160804192533) do
     t.string   "name"
     t.text     "address"
     t.string   "email"
-    t.string   "pay_type"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "payment_type_id"
